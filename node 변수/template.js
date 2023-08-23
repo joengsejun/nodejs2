e56b25f471dac4b519bf1b91b68f87bbe143ffdd
jeongsejun <a01066017161@gmail.com>
2023-08-23 19:38:48 +0900

    file

---
+++ node 변수/template.js
@@ -0,0 +1,5 @@
+var name = 'egoing';
+var letter = 'Dear '+name+'\
+\
+ orem ipsum dolor sit amet, comsectetur adipisicing elit, sed do eiusmod tempor incididunt ot labore et dolore magna aliqua. '+name+' Utenim ad minim veniam, quis nostrud exercitation.';
+console.log(letter);
---
+++ node 변수/variable2.js
@@ -0,0 +1,3 @@
+var name = 'egoing';
+var letter = 'Dear '+name+' Lorem ipsum dolor sit amet, comsectetur adipisicing elit, sed do eiusmod tempor incididunt ot labore et dolore magna aliqua. '+name+' Utenim ad minim veniam, quis nostrud exercitation.';
+console.log(letter); 
