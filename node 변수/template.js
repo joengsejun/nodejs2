7712ac034ae9e1cff69605818636532250707780
jeongsejun <a01066017161@gmail.com>
2023-08-23 20:04:53 +0900

    file

--- node 변수/template.js
+++ node 변수/template.js
@@ -1,5 +1,9 @@
 var name = 'egoing';
-var letter = 'Dear '+name+'\
-\
- orem ipsum dolor sit amet, comsectetur adipisicing elit, sed do eiusmod tempor incididunt ot labore et dolore magna aliqua. '+name+' Utenim ad minim veniam, quis nostrud exercitation.';
+var letter = 'Dear '+name+'\n\nLorem ipsum dolor sit amet, comsectetur adipisicing elit, sed do eiusmod tempor incididunt ot labore et dolore magna aliqua. '+name+' Utenim ad minim veniam, quis nostrud exercitation.';
+console.log(letter);
+
+var name = 'egoing';
+var letter = `Dear ${name}
+
+Lorem ipsum dolor sit amet, comsectetur adipisicing elit, sed do eiusmod tempor incididunt ot labore et dolore magna aliqua. ${name} Utenim ad minim veniam, quis nostrud exercitation.`;
 console.log(letter);
