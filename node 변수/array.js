87e9f31b1f0570ef842e3f50a0d49b56ff04ea99
jeongsejun <a01066017161@gmail.com>
2023-09-11 20:38:24 +0900

    file

---
+++ node 변수/array.js
@@ -0,0 +1,9 @@
+//array 리터럴
+var arr = ['A','B','C','D',12,true];
+console.log(arr[1]);
+console.log(arr[3]);
+arr[2]=3;
+console.log(arr);
+console.log(arr.length);
+arr.push('E');
+console.log(arr);
