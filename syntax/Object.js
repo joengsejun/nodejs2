de1f5fb8a890db01c592776533b37db049cc3c9c
jeongsejun <a01066017161@gmail.com>
2023-10-18 19:49:20 +0900

    update Object

---
+++ syntax/Object.js
@@ -0,0 +1,5 @@
+var members = ['egoing', 'k8805', 'hoya'];//Array
+console.log(members[1]); //k8805
+
+var roles = {'programer': 'egoing', 'designer':'k8805', 'manager':'hoya'};//Object
+console.log(roles.designer); //k8805
